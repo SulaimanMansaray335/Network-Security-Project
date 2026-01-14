d321cee48e6cbb3da665c46f0a465d0af7c62396
SulaimanMansaray335 <Sulaimant.mansaray@gmail.com>
2026-01-13 22:14:22 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List 
+
+def get_requirements()-> List[str]: 
+
+    requirements_list:List[str] = [] 
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement!= '-e .':
+                    requirements_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirements_list  
+
+
+setup(
+    name= "Networ_Security_Project",
+    version= "0.0.1",
+    author= "Tariq Mansaray",
+    author_email= "Tariq.Mansaray@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
+                
+
