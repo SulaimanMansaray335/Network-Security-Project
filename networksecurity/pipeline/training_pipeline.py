19eb72eb9fcade9db7f5285ecedccfe7e42cb165
SulaimanMansaray335 <Sulaimant.mansaray@gmail.com>
2026-01-19 11:54:19 -0500

    added more

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -72,14 +72,14 @@ class TrainingPipeline:
     def sync_artifact_to_s3(self):
         try:
             aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync.folder_to_s3(folder = self.training_pipeline_config.artifact_dir, aws_bucket_url = aws_bucket_url)
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir, aws_bucket_url = aws_bucket_url)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
     def sync_saved_model_to_s3(self):
         try:
             aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync.folder_to_s3(folder = self.training_pipeline_config.model_dir, aws_bucket_url = aws_bucket_url)
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir, aws_bucket_url = aws_bucket_url)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
